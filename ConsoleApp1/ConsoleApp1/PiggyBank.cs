57dce3c3daecafd64c3b058229a56ce0d7235f44
Dhruv <dgupta@muellerwp.com>
2020-08-27 11:36:50 -0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/PiggyBank.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+public delegate void EventListner(int value);
+
+public class PiggyBank
+{
+	int currBalance;
+	public event EventListner currBalanceUpdate;
+
+	public PiggyBank()
+	{
+		currBalance = 0;
+	}
+
+	public void deposit(int val) {
+		currBalance += val;
+		currBalanceUpdate(currBalance);
+
+	}
+
+	public void withdraw(int val) {
+		currBalance -= val;
+	}
+
+
+}
+
+class balanceLogger {
+	public void printBalance(int val)
+	{
+		Console.WriteLine("Current balance: " + val.ToString());
+	}
+}
+
+class balanceWatcher
+{ 
+	public void printBalance(int val)
+	{
+		if (val > 500)
+		{
+			Console.WriteLine("You reached savings goal! Current balance: " +
+				val.ToString()
+				);
+		}
+		
+	}
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            PiggyBank acc = new PiggyBank();
+            balanceLogger bl = new balanceLogger();
+            balanceWatcher bw = new balanceWatcher();
+
+            acc.currBalanceUpdate += bl.printBalance;
+            acc.currBalanceUpdate += bw.printBalance;
+
+            int dep = 0;
+
+            while (true)
+            {
+                Console.WriteLine("How much to deposit?");
+                string val = Console.ReadLine();
+                if (val.Trim().ToLower() == "exit")
+                    break;
+
+                int.TryParse(val, out dep);
+                acc.deposit(dep);
+
+                
+
+            }
+
+
+
+        }
+
+    }
+
+
+}
